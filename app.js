d2f4bbe892574f74d2aa3c08dd7ae28d776a7ab6
Russ <rperry1886@gmail.com>
2026-09-08 11:29:59 -0400

    Add visualizer

---
+++ app.js
@@ -0,0 +1,33 @@
+const projectName = "[Music Quiz]";
+
+// Audio Visualizer
+const audio = document.querySelector("#quizAudio");
+const bars = document.querySelectorAll(".bar");
+
+const audioContext = new AudioContext();
+const analyzer = audioContext.createAnalyser();
+
+analyzer.fftSize = 256;
+
+const source = audioContext.createMediaElementSource(audio);
+source.connect(analyzer);
+analyzer.connect(audioContext.destination);
+
+const data = new Uint8Array(analyzer.frequencyBinCount);
+
+function animate() {
+  analyzer.getByteFrequencyData(data);
+
+  bars.forEach((bar, index) => {
+    const value = data[index];
+    const height = Math.max(5, (value / 255) * 100);
+    bar.style.height = `${height}px`;
+  });
+
+  requestAnimationFrame(animate);
+}
+
+audio.addEventListener("play", () => {
+  audioContext.resume();
+  animate();
+});
--- readme.md
+++ readme.md
@@ -5,3 +5,11 @@ An app where you can use your local music files to do a name that tune style qui
 ## Music
 
 Currently, there are 5 songs from Baroness. These are intended to just be for testing, and will be removed once testing is done.
+
+## Change Log
+
+### 9/8/26
+
+---
+
+- Added a visualizer for the song that reacts to the current song playing.
